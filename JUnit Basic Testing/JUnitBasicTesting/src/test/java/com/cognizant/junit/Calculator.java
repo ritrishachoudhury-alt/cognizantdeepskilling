27fc8727b9c8932e08dce50bb5baa76a9caae532
Ritrisha Choudhury <YOUR_GITHUB_EMAIL>
2026-07-26 22:01:51 +0530

    Added JUnit Basic Testing exercises

---
+++ JUnit Basic Testing/JUnitBasicTesting/src/test/java/com/cognizant/junit/Calculator.java
@@ -0,0 +1,10 @@
+
+package com.cognizant.junit;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+}
---
+++ JUnit Basic Testing/JUnitBasicTesting/src/test/java/com/cognizant/junit/CalculatorTest.java
@@ -0,0 +1,42 @@
+package com.cognizant.junit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    Calculator calculator;
+
+    @Before
+    public void setUp() {
+
+        calculator = new Calculator();
+
+    }
+
+    @Test
+    public void testAdd() {
+
+        // Arrange
+        int a = 10;
+        int b = 20;
+
+        // Act
+        int result = calculator.add(a, b);
+
+        // Assert
+        assertEquals(30, result);
+
+    }
+
+    @After
+    public void tearDown() {
+
+        calculator = null;
+
+    }
+
+}
